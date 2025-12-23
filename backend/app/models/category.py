0d3371eaded1fb08a19b94ec62c0f2c5dc688bcc
Mac <mac@MacBook-Pro.local>
2025-12-24 01:05:15 +0800

    creer categorie

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -29,6 +29,7 @@ class Category(BaseModel):
     model_config = {
         "populate_by_name": True,
         "arbitrary_types_allowed": True,
+        "json_encoders": {ObjectId: str},
     }
 
 class CategoryCreate(BaseModel):
